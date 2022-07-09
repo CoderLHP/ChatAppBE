08c87339e89113b9461ff40a6ab909512b8fbbbc
chung080702 <phamquangchung550@gmail.com>
2022-07-09 23:25:18 +0700

    Chung_220709_13_1

---
+++ src/main/java/coder/chatApp/Constant.java
@@ -0,0 +1,8 @@
+package coder.chatApp;
+
+public class Constant {   
+    public static final String mysqlURL = "jdbc:mysql://localhost:3306/";
+    public static final String database = "ChatApp";
+    public static final String username = "root";
+    public static final String password = "chung080702";
+}
